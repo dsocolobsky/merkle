16bbc17874b88f6952417f3cd9644e27e956e5ff
Dylan Socolobsky <dylan.socolobsky@lambdaclass.com>
2024-11-14 15:41:13 -0300

    Rename badly named parameters

--- src/main.rs
+++ src/main.rs
@@ -8,22 +8,22 @@ fn contains_hash(merkle: &MerkleTree, hash: &String) -> bool {
 }
 
 /// Creates the next (upper) level for a Merkle Tree given the previous level.
-fn calculate_next_level(vec: &[String]) -> Vec<String> {
+fn calculate_next_level(prev_level: &[String]) -> Vec<String> {
     let mut next_level: Vec<String> = vec![];
-    if vec.is_empty() {
+    if prev_level.is_empty() {
         return next_level;
     }
-    for i in (0..vec.len()-1).step_by(2) {
-        let hash = hash_multiple_str(vec![vec[i].clone(), vec[i+1].clone()]);
+    for i in (0..prev_level.len()-1).step_by(2) {
+        let hash = hash_multiple(vec![prev_level[i].clone(), prev_level[i+1].clone()]);
         next_level.push(hash);
     }
     next_level
 }
 
 /// Creates the bottom level (leafs) for a Merkle Tree given a vector of u32
-fn create_initial_level(vec: &[u32]) -> Vec<String> {
+fn create_initial_level(initial_vals: &[u32]) -> Vec<String> {
     let mut res: Vec<String> = vec![];
-    for &i in vec.iter() {
+    for &i in initial_vals.iter() {
         let hash = Keccak256::digest(i.to_le_bytes());
         let hash = hex::encode(hash);
         res.push(hash);
@@ -32,8 +32,8 @@ fn create_initial_level(vec: &[u32]) -> Vec<String> {
 }
 
 /// Creates a Merkle Tree given a vector of u32
-fn create_merkle_tree(vec: &[u32]) -> MerkleTree {
-    let initial = create_initial_level(vec);
+fn create_merkle_tree(initial_vals: &[u32]) -> MerkleTree {
+    let initial = create_initial_level(initial_vals);
     let mut actual: Box<Vec<String>> = Box::new(initial);
     let mut levels: MerkleTree = vec![];
     levels.push(*actual.clone());
@@ -50,10 +50,10 @@ fn hash_one(n: u32) -> String {
 }
 
 /// Returns the digest for several strings
-fn hash_multiple_str(vec: Vec<String>) -> String {
+fn hash_multiple(hashes: Vec<String>) -> String {
     let mut hasher = Keccak256::new();
-    for s in vec.iter() {
-        sha3::Digest::update(&mut hasher, s);
+    for h in hashes.iter() {
+        sha3::Digest::update(&mut hasher, h);
     }
     hex::encode(hasher.finalize())
 }
@@ -82,13 +82,13 @@ mod tests {
 
         let three_hash = hash_one(3);
         let four_hash = hash_one(4);
-        let threefour_hash = hash_multiple_str(vec![three_hash.clone(), four_hash.clone()]);
+        let threefour_hash = hash_multiple(vec![three_hash.clone(), four_hash.clone()]);
 
         let five_hash = hash_one(5);
         let six_hash = hash_one(6);
-        let fivesix_hash = hash_multiple_str(vec![five_hash.clone(), six_hash.clone()]);
+        let fivesix_hash = hash_multiple(vec![five_hash.clone(), six_hash.clone()]);
 
-        let root = hash_multiple_str(vec![threefour_hash.clone(), fivesix_hash.clone()]);
+        let root = hash_multiple(vec![threefour_hash.clone(), fivesix_hash.clone()]);
 
         assert!(contains_hash(&tree, &three_hash));
         assert!(contains_hash(&tree, &four_hash));
