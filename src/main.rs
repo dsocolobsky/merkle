7ef278dd7503fdb651090f43628e0e02a0e010f7
Dylan Socolobsky <dylan.socolobsky@lambdaclass.com>
2024-11-14 15:38:44 -0300

    verify_proof

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,32 @@ use sha3::{Digest, Keccak256};
 
 type MerkleTree = Vec<Vec<String>>;
 
+/// Returns the index in the leaf array for a given element, if present.
+fn leaf_index_for_element(merkle: &MerkleTree, element: u32) -> Option<(String, usize)> {
+    let hash = hash_one(element);
+    if let Some(index) = merkle[0].iter().position(|h| *h == hash) {
+        Some((hash, index))
+    } else {
+        None
+    }
+}
+
+/// Generates a proof that a certain element belongs to the tree, if present.
+fn verify_proof(element: u32, index: usize, proof: &[String], root: &String) -> bool {
+    let mut hash = hash_one(element);
+    let mut index = index;
+
+    for p in proof.iter() {
+        hash = if index % 2 == 0 {
+            hash_multiple(vec![hash.clone(), p.clone()])
+        } else {
+            hash_multiple(vec![p.clone(), hash.clone()])
+        };
+        index = index / 2;
+    }
+    hash == *root
+}
+
 /// Checks if the given hash is present at any level of the Merkle Tree
 fn contains_hash(merkle: &MerkleTree, hash: &String) -> bool {
     merkle.iter().any(|level| level.contains(hash))
@@ -90,6 +116,12 @@ mod tests {
 
         let root = hash_multiple(vec![threefour_hash.clone(), fivesix_hash.clone()]);
 
+        // Test we can get the leaf index for each element
+        assert_eq!(leaf_index_for_element(&tree, 5).unwrap().1, 2);
+        assert_eq!(leaf_index_for_element(&tree, 6).unwrap().1, 3);
+        assert_eq!(leaf_index_for_element(&tree, 11), None);
+
+        // Test contains_hash
         assert!(contains_hash(&tree, &three_hash));
         assert!(contains_hash(&tree, &four_hash));
         assert!(contains_hash(&tree, &threefour_hash));
@@ -97,7 +129,14 @@ mod tests {
         assert!(contains_hash(&tree, &six_hash));
         assert!(contains_hash(&tree, &fivesix_hash));
         assert!(contains_hash(&tree, &root));
-
         assert!(!contains_hash(&tree, &hash_one(7)));
+
+        // Test verify_proof for even index
+        assert!(verify_proof(3, 0, &[four_hash.clone(), fivesix_hash.clone()], &root));
+        assert!(!verify_proof(9, 0, &[four_hash.clone(), fivesix_hash.clone()], &root));
+
+        // Test verify_proof for odd index
+        assert!(verify_proof(6, 3, &[five_hash.clone(), threefour_hash.clone()], &root));
+        assert!(!verify_proof(10, 3, &[five_hash.clone(), threefour_hash.clone()], &root));
     }
 }
